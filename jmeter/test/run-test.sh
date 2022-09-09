20157562cdb7b06294b62ef17af16b5a9259c634
Stephan Zeissler <stephan@stormforger.com>
2022-09-09 16:51:47 +0200

    Use tee to write the logfile but also get output to stdout

--- jmeter/test/run-test.sh
+++ jmeter/test/run-test.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-#
-# This script builds a new docker image, runs a load test once 
-#
-
-
 set -eu -o pipefail
 
 assert_logs_contains() {
@@ -17,10 +12,9 @@ LOGS_TEMPFILE="$(mktemp)"
 
 # docker build -t "${IMAGE_NAME}" "$(dirname $0)/.."
 
-docker run --rm --name "${CONTAINER_NAME}" -ti -v "$(pwd)/$(dirname $0):/test:ro" "${IMAGE_NAME}" 2>&1 > $LOGS_TEMPFILE
+docker run --rm --name "${CONTAINER_NAME}" -ti -v "$(pwd)/$(dirname $0):/test:ro" "${IMAGE_NAME}" 2>&1 | tee $LOGS_TEMPFILE
 
 test -f $LOGS_TEMPFILE
-cat $LOGS_TEMPFILE
 
 assert_logs_contains "errorCount" # we have stats
 assert_logs_contains "errorPct"
