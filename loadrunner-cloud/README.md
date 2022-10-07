60973a7e247ca6740870e3b1487d3d7d1a247bc7
Sebastian Cohnen <tisba@users.noreply.github.com>
2022-10-07 10:38:55 +0200

    adds initial stuff for LoadRunner Cloud CLI

---
+++ loadrunner-cloud/README.md
@@ -0,0 +1,20 @@
+# LoadRunner Cloud Trial Image
+
+This trial image contains the LoadRunner Java CLI, which can be found [here](https://admhelp.microfocus.com/lrc/en/2022.06/Content/Storm/t_cli_tools.htm).
+
+## Configuration
+
+| Environment Variable | Description |
+| -------------------- | ----------- |
+| `LOADRUNNER_CLOUD_CONNECT`        | Your credentials to log into LoadRunner Cloud. |
+| `LOADRUNNER_CLOUD_HOST`    | The URL of your LoadRunner Cloud tenant. |
+| `LOADRUNNER_CLOUD_TENANTID`    | Your tenant id, specified in your LoadRunner Cloud URL. |
+| `LOADRUNNER_CLOUD_TESTID`    | The ID of the test. |
+
+## Metrics
+
+TODO: …
+
+## Example Kubernetes Manifest
+
+TODO: …
---
+++ loadrunner-cloud/docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+if [ -z "${LOADRUNNER_CLOUD_CONNECT}" ]; then
+  echo "Missing LOADRUNNER_CLOUD_CONNECT"
+  exit 1
+fi
+
+if [ -z "${LOADRUNNER_CLOUD_HOST}" ]; then
+  echo "Missing LOADRUNNER_CLOUD_HOST"
+  exit 1
+fi
+
+if [ -z "${LOADRUNNER_CLOUD_TENANTID}" ]; then
+  echo "Missing LOADRUNNER_CLOUD_TENANTID"
+  exit 1
+fi
+
+if [ -z "${LOADRUNNER_CLOUD_TESTID}" ]; then
+  echo "Missing LOADRUNNER_CLOUD_TESTID"
+  exit 1
+fi
+
+cli () {
+    java -jar /loadrunner/jar/cli.jar \
+        "$1" \
+        "testId=${LOADRUNNER_CLOUD_TESTID}" \
+        "connect=${LOADRUNNER_CLOUD_CONNECT}" \
+        sendEmail=False
+}
+
+# https://admhelp.microfocus.com/lrc/en/2022.06/Content/Storm/t_cli_tools.htm
+cli "runWithResult"
+cli "status"
+cli "getResult"
+
