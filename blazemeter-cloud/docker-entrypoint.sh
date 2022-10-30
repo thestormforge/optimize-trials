a5acea5317754c7a75c6f9635968536afcee9ff5
Zach Reinhart <zach@stormforge.io>
2022-10-30 14:04:03 -0500

    Entrypoint script now works; cleaning up bzt-rc

--- blazemeter-cloud/docker-entrypoint.sh
+++ blazemeter-cloud/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xe
+set -e
 
 if [ -z "${BLAZEMETER_API_ID}" ]; then
 	echo "ERROR: BLAZEMETER_API_ID not found. An API ID and an API Secret are required to access Blazemeter." > /dev/stderr
@@ -30,8 +30,8 @@ bzt "${args[@]}"
 
 # Push the basic statistics
 if [ -n "${PUSHGATEWAY_URL}" ]; then
-	mlr --no-auto-unflatten --c2j cut -x -f label /tmp/final-stats.csv \
-		| jq -r '.[0]|keys[] as $k | "\($k) \(.[$k])"' \
+	mlr --c2j remove-empty-columns then rename -g -r '\.0,' /tmp/final-stats.csv \
+		| jq -r 'keys[] as $k | "\($k) \(.[$k])"' \
 		| curl --data-binary @- "${PUSHGATEWAY_URL}"
 else
 	echo "WARN: No PUSHGATEWAY_URL configured" > /dev/stderr
